95412a47ccea0ca85259ea38fb97d51593a4a2db
nutika2025 <nutika2025@gmail.com>
2025-12-03 22:25:59 -0700

    Update ticker top bar to black background with white text

--- impact-post-next/src/components/layout/ticker.tsx
+++ impact-post-next/src/components/layout/ticker.tsx
@@ -1,6 +1,6 @@
 export function Ticker() {
   return (
-    <div className="ticker-wrap border-b-2 border-black z-50 relative bg-white">
+    <div className="ticker-wrap border-b-2 border-black z-50 relative bg-black text-white">
       <div className="ticker font-mono font-bold text-sm tracking-widest uppercase py-2">
         +++ BREAKING: New Cultural Centre Approved in Etobicoke +++ Youth Scholarship Applications Open until Nov 30 +++ Community Business Awards Nominations Now Open +++
       </div>
